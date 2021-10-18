bbfbb97dda51b47bc15c1d8edfcdd80e3dad5e66
shishir1809019 <shishirdas726@gmail.com>
2021-10-18 20:43:10 +0600

    solve reload problem

--- src/components/Home/MediDetails/MediDetails.js
+++ src/components/Home/MediDetails/MediDetails.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Button } from "react-bootstrap";
+import { useHistory, useParams } from "react-router";
 
 const MediDetails = () => {
   const { id } = useParams();
 
+  const history = useHistory();
   const [medicines, setMedicines] = useState([]);
+
+  const handleDetailButton = () => {
+    history.push("/home");
+  };
 
   useEffect(() => {
     fetch("/fakeData.json")
@@ -22,6 +28,9 @@ const MediDetails = () => {
         <h3>{selectedMedicine[0]?.name}</h3>
         <p>Price: {selectedMedicine[0]?.price} tk/pice</p>
         <p>{selectedMedicine[0]?.description}</p>
+        <Button variant="outline-dark" onClick={handleDetailButton}>
+          Back to Services
+        </Button>
       </div>
     </div>
   );
