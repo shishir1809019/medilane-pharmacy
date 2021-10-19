1f857cfa6e3cc47113199a250413cd618f2ce049
shishir1809019 <shishirdas726@gmail.com>
2021-10-19 17:19:09 +0600

    notfound page edit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from "react";
+import img from "../../images/404page.jpg";
 
 const NotFound = () => {
   return (
-    <div>
-      <h3>Page not found</h3>
+    <div
+      style={{ background: "#f1f1f1" }}
+      className="d-flex justify-content-center py-5"
+    >
+      <img src={img} alt="" />
     </div>
   );
 };
